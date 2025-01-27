3f48c766eb1ca91a08c80b897ac5199078974907
jvkhop <168108621+jvkhop@users.noreply.github.com>
2025-01-27 23:42:45 +0700

    Almost Done!
    Yup!

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('click', function() {
+    let password = prompt("Always🐣...");
+    if (password === "duyanhluvemmm") {
+        window.location.href = "./second_page/index.html";
+    } else {
+        alert("Anh cắn em bây giờ 🙃");
+    }
+});
